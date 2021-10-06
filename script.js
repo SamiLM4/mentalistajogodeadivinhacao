5a078195e784245ca950a9d3a6ef698baf3f18d1
SamiLM4 <91291453+SamiLM4@users.noreply.github.com>
2021-10-06 07:41:40 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function Chutar() {
+  var numeroSecreto = parseInt(Math.random() * 11);
+  var elementoResultado = document.getElementById("resultado");
+  var chute = parseInt(document.getElementById("valor").value);
+  console.log(chute);
+  if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = "você acertou !!";
+    console.log("acertou");
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = "você deve digitar um número de 0 a 10 !!";
+    console.log("você deve digitar um número de 0 a 10");
+  } else {
+    elementoResultado.innerHTML =
+      "você errou ! não foi dessa vez, o número secreto era: " + numeroSecreto;
+    console.log("errou");
+  }
+
+  if (chute < numeroSecreto) {
+    elementoResultado.innerHTML = "você chutou um numero muito baixo";
+  } else if (chute > numeroSecreto) {
+    elementoResultado.innerHTML = "você chutou um numero muito alto";
+  }
+}
